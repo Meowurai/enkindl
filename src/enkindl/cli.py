c34575b2d619ee80c7a809a24c7fce2e7ba57120
Caj Michael Andersson <cajmandersson@icloud.com>
2026-03-04 19:13:04 +0100

    add argparse CLI with init and explain subcommands

--- src/enkindl/cli.py
+++ src/enkindl/cli.py
@@ -1,4 +1,27 @@
 
+import argparse 
 
 def main():
-    print("enkindl is alive!")
+    parser = argparse.ArgumentParser(
+        prog="enkindl",
+        description="A tiny CLI tool that scaffolds Python projects for beginners."
+    )
+
+    subparsers = parser.add_subparsers(dest="command")
+
+    # enkindl init <project_name>
+    init_parser = subparsers.add_parser("init", help="Scaffold a new Python project")
+    init_parser.add_argument("name", help="Name of the project to create")
+
+    # enkindl explain <topic>
+    explain_parser = subparsers.add_parser("explain", help="Explain a Python concept")
+    explain_parser.add_argument("topic", help="Topic to explain")
+
+    args = parser.parse_args()
+
+    if args.command == "init":
+        print(f"Creating project: {args.name}")
+    elif args.command == "explain":
+        print(f"Explaining: {args.topic}")
+    else:
+        parser.print_help()
